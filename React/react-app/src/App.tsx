fb76e43d9c474f89a26cbe6ba928a75033accf95
Harshada Karane <harshada.karane@sap.com>
2023-04-02 11:58:47 +0530

    Basic react app
    Basic react app

---
+++ React/react-app/src/App.tsx
@@ -0,0 +1,8 @@
+import Message from "./Message";
+
+function App(){
+  //return <div><Message /></div>       //creates additional div tag which can be ignored 
+  return <Message></Message>
+}
+
+export default App;
