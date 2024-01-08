49f0b2f13c91513f6f88e064c2761b2f75ee4709
Harshada Karane <harshada.karane@sap.com>
2024-01-08 09:25:42 +0530

    Basic concepts
    Basic concepts

--- React/hello-world/src/index.js
+++ React/hello-world/src/index.js
@@ -8,6 +8,36 @@ import ReactDOM from 'react-dom/client';
 // )
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
+/* -------------- add multiple JSX elements using div (not recommended as creates additional div tag) -------------- */
+// root.render(
+//   <div>
+//     <h1>Hello World</h1>
+//     <h3>welcome..</h3>
+//   </div>
+// )
+
+/* -------------- add multiple JSX elements using square brackets -------------- */
+// root.render(
+//   [
+//     <h1>Hello World</h1>,
+//     <h3>welcome..</h3>
+//   ]
+// )
+
+
+/* -------------- add multiple JSX elements using fragment <> -------------- */
+// root.render(
+//   <>
+//     <h1>Hello World</h1>
+//     <h3>welcome...</h3>
+//   </>
+// )
+
+
 root.render(
-  <h1>Hello World</h1>
+  <>
+    <h1>Hello World</h1>
+    <h3>{`Current Date : ${new Date().toLocaleDateString()}`}</h3>
+    <h4>{`Current Time : ${new Date().toLocaleTimeString()}`}</h4>
+  </>
 )
