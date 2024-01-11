d11f4fbe11854359265d35cc7aaa881e8cdd2ed6
Harshada Karane <harshada.karane@sap.com>
2024-01-11 08:57:47 +0530

    import export example
    import export example

---
+++ React/import-export/src/App.jsx
@@ -0,0 +1,15 @@
+const fruit = "Mango";
+
+const juice = "Watermelon";
+
+function welcome(){
+    let msg = "Welcome back"
+    return msg;
+}
+
+export default fruit;
+
+export {juice, welcome};
+
+
+//Only one default export is allowed
