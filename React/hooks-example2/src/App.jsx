c65ecbea48726ee61ce33d38614968b5cf097ade
Harshada Karane <harshada.karane@sap.com>
2024-01-25 13:22:13 +0530

    Digital clock example using hooks
    Digital clock example using hooks

--- React/hooks-example2/src/App.jsx
+++ React/hooks-example2/src/App.jsx
@@ -2,16 +2,28 @@ import React, { useState } from "react";
 import './App.css'
 
 const App = () => {
-    let sCurrentTime = new Date().toLocaleTimeString();
-    const [sTime, setCurrentTime] = useState(sCurrentTime)
+    let sCurrentTime = new Date().toLocaleTimeString("en-US");
+    const [sTime, setCurrentTime] = useState(sCurrentTime);
+    
     const onBtnClick = () => {
-        sCurrentTime = new Date().toLocaleTimeString();
+        sCurrentTime = new Date().toLocaleTimeString("en-US");
         setCurrentTime(sCurrentTime);
     }
+
+    //let t = new Date().toLocaleTimeString("en-US");
+    const [sTimeString, setTimeString] = useState(sCurrentTime)
+    const updateTime = () => {
+        sCurrentTime = new Date().toLocaleTimeString("en-US");
+        setTimeString(sCurrentTime);
+    }
+    setInterval(updateTime, 1000);
    return (
        <div style={{textAlign : 'center'}}>
            <h1> {sTime} </h1>
            <button className="btn" onClick={onBtnClick}>Click Me</button>
+            <hr />
+            <h1> {sTimeString} </h1>
+
        </div>
    )
 }
