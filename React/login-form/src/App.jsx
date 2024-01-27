da3824bef00001f1746a8fba2c29fa09dc63b5af
Harshada Karane <harshada.karane@sap.com>
2024-01-27 15:57:38 +0530

    Multiple input example
    Multiple input example

--- React/login-form/src/App.jsx
+++ React/login-form/src/App.jsx
@@ -2,20 +2,38 @@ import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
-  const [sName, setName] = useState("");  
-  const [sFullName, setFullName] = useState();
+  const [fName, setFname] = useState("");  
+  const [lName, setLname] = useState("");  
+  const [firstName, setFirstName] = useState();
+  const [lastname, setLastname] = useState();
    
   const onInputChange = (oEvent) => {
-    setName(oEvent.target.value)
+    setFname(oEvent.target.value)
   }
-  const onBtnClick = () => {
-    setFullName(sName);
+  const onLastNameChange = (oEvent) => {
+    setLname(oEvent.target.value);
+  }
+  const onBtnClick = (oEvent) => {
+    oEvent.preventDefault();    //By default 'form's' submit method refreshes the page. To prevent this behaviour use 'preventDefault()'
+    setFirstName(fName);
+    setLastname(lName);
   } 
   return (
-    <div>
+    // ---------------- without form tag ---------------------------------
+    /* <div>
         <h1>Hello { sFullName }</h1>
         <input placeholder='Enter Name' onChange={ onInputChange } value={sName}/>
         <button onClick={ onBtnClick }> Click Me</button>
+    </div> */
+    // ---------------- with form tag ---------------------------------
+    <div>
+      <form onSubmit={onBtnClick}>
+        <h1>Hello { firstName } {lastname}</h1>
+        <input placeholder='Enter First Name' onChange={ onInputChange } value={fName}/>
+        <br />
+        <input placeholder='Enter Last Name' onChange={ onLastNameChange } value={lName}/>
+        <button type='submit'> Click Me</button>
+      </form>
     </div>
   )
 }
